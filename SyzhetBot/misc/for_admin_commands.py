7e93fd67e7b21c23f1c83fe23bf43a35121c7b50
Алексей <click742@yandex.ru>
2022-12-11 19:59:53 +0300

    Update certifi from 2022.6.15.2 to certifi-2022.12.7

--- SyzhetBot/misc/for_admin_commands.py
+++ SyzhetBot/misc/for_admin_commands.py
@@ -9,16 +9,16 @@ async def list_user_to_message(message: types.Message, response: List):
         if resp['orders']:
             for ord in resp['orders']:
                 orders.append(
-                    (f'id: {ord["id"]}\n'
-                        f'Название: {ord["title"]}\n'
-                        f'Описание: {ord["description"]}\n'
-                        '-----------\n')
+                    (f'id заказа: {ord["id"]}\n'
+                     f'Название: {ord["title"]}\n'
+                     f'Описание: {ord["description"]}\n'
+                     '-----------\n')
                 )
             orders = ''.join(orders)
         else:
             orders = 'Нет заказов'
         await message.answer(
-            (f'id: {resp["id"]}\n'
+            (f'id пользователя: {resp["id"]}\n'
              f'usernmae: @{resp["username"]}\n'
              f'telegram_id: {resp["telegram_id"]}\n'
              f'updated: {resp["updated_on"]}\n'

cfa2f50e1394df21216cca643823aa8856dd35a2
Алексей <click742@yandex.ru>
2022-12-12 19:32:41 +0300

    Add function get_order_from_id in ApiHttpRequest. Add order_obj_to_message in for_admin_commands.py. Add cmd_order_id in admin_cmd.py

--- SyzhetBot/handlers/admin/admin_cmd.py
+++ SyzhetBot/handlers/admin/admin_cmd.py
@@ -5,7 +5,7 @@ from ...filters.admin import AdminFilter
 from ...misc.for_admin_commands import (list_order_to_message,
                                         list_user_to_message,
                                         order_obj_to_message,
-                                        user_obj_to_message,)
+                                        user_obj_to_message)
 from ...misc.http_request import ApiHttpRequest
 
 USER_URL = '/api/v1/users/'
